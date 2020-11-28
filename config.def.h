0f44f1015d7c0a539da9f3db9768ff8ccfe49fed
Nathaniel Evan <nathanielevan@zohomail.com>
2020-11-29 03:38:10 +0700

    Applies patch st-font2-20190416-ba72400.diff and adds JoyPixels for emoji font

--- config.def.h
+++ config.def.h
@@ -6,6 +6,10 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Iosevka Nerd Font Mono:pixelsize=16:antialias=true:autohint=true";
+/* Spare fonts */
+static char *font2[] = {
+	"JoyPixels:pixelsize=16:antialias=true:autohint=true",
+};
 static int borderpx = 15;
 
 /*
