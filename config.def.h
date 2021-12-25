19115773cca379d3a9d0465a963d477f3c30924d
Nathaniel Evan <nathanielevan@zohomail.com>
2021-12-25 23:51:45 +0700

    Uploads new colour scheme

--- config.def.h
+++ config.def.h
@@ -112,30 +112,30 @@ unsigned int tabspaces = 8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"#3b4252", /* black   */
-	"#bf616a", /* red     */
-	"#a3be8c", /* green   */
-	"#ebcb8b", /* yellow  */
-	"#81a1c1", /* blue    */
-	"#b48ead", /* magenta */
-	"#88c0d0", /* cyan    */
-	"#e5e9f0", /* white   */
+	"#192e40", /* black   */
+	"#c20f47", /* red     */
+	"#25a37e", /* green   */
+	"#e9986d", /* yellow  */
+	"#5d92bf", /* blue    */
+	"#6768bf", /* magenta */
+	"#5accc5", /* cyan    */
+	"#fff6e6", /* white   */
 
 	/* 8 bright colors */
-	"#4c566a", /* black   */
-	"#bf616a", /* red     */
-	"#a3be8c", /* green   */
-	"#ebcb8b", /* yellow  */
-	"#81a1c1", /* blue    */
-	"#b48ead", /* magenta */
-	"#8fbcbb", /* cyan    */
-	"#eceff4", /* white   */
+	"#284259", /* black   */
+	"#c20f47", /* red     */
+	"#25a37e", /* green   */
+	"#e9986d", /* yellow  */
+	"#5d92bf", /* blue    */
+	"#6768bf", /* magenta */
+	"#5accc5", /* cyan    */
+	"#fffef8", /* white   */
 
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
-	"#2e3440", /* background */
-	"#d8dee9", /* foreground */
+	"#0b1726", /* background */
+	"#fff6e6", /* foreground */
 };
 
 
