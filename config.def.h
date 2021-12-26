bd8832a072081696b8c5997e67943ca87d410be0
Nathaniel Evan <nathanielevan@zohomail.com>
2021-12-26 18:58:59 +0700

    Change font to InconsolataGo

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Inconsolata Nerd Font Mono:size=12:antialias=true:autohint=true";
+static char *font = "InconsolataGo Nerd Font Mono:size=12:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
 	"Noto Color Emoji:pixelsize=12:antialias=true:autohint=true",
